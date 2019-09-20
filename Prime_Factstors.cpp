83f36fa635fa46d5e6dcace414d0f6736c8dbd5f
tungptiter <tungptiter@gmail.com>
2019-09-20 22:52:29 +0700

    uoc so nguyen to cua n

--- Prime_Factstors.cpp
+++ Prime_Factstors.cpp
@@ -5,15 +5,21 @@ using namespace std;
 int main()
 {
     long long n;
-    cout<<"Nhap n: ";
-    cin>>n;
-    long i = 2;
-    while(n > 1){
-        if (n%i == 0){
-            n /= i;
-            cout<<i <<" ";
+    int test;
+    // cout<<"TEST: ";
+    cin>>test;
+    while(test--){
+        // cout<<"Nhap n: ";
+        cin>>n;
+        long i = 2;
+        while(n > 1){
+            if (n%i == 0){
+                n /= i;
+                cout<<i <<" ";
+            }
+            else i++;
         }
-        else i++;
+        cout<<"\n";
     }
 }
 
